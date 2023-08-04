4a991639ecf6133e36ce5cb2ab8ddee487808bca
Laurenz Haase <laurenz.haase@gmx.de>
2023-08-04 14:01:01 +0200

    YEET

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+enum SHIRTCOLOR {
+    RED,
+    BLUE,
+}
+
+struct NPC{
+    name:String,
+    shirt_color:SHIRTCOLOR,
+    position:(i32,i32),
+    iq:Option<bool>,
+
+}
+
+impl NPC{
+    fn to_String(){
+
+    }
+    
+    fn moving(){
+
+    }
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    
+    
+    
 }
